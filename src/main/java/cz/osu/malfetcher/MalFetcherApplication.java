ddfa3c7052ac172378f329a9983d19a33b402280
datre9 <david.trembacz@gmail.com>
2024-06-24 10:28:47 +0200

    Initial commit

---
+++ src/main/java/cz/osu/malfetcher/MalFetcherApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.malfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalFetcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/osu/malfetcher/MalFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.malfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
